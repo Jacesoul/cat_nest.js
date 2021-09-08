c7ca09d0f4d2c891170a02e6b582a757d74fa09d
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-08 12:28:59 +0900

    Modules& Middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer는 소비자이다. LoggerMiddleware는 의존성 주입이 가능한 provider이다.
+    consumer.apply(LoggerMiddleware).forRoutes('cats'); // cats 라우터에 바인딩을 해주는것이다. * 를 사용하면 전체 엔드포인트에대해 logger가 실행된다.
+  }
+}
---
+++ src/logger.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    // this.logger.log(`${req.ip}, ${req.originalUrl}, ${req.method}`);
+    res.on('finish', () => {
+      // response가 완료가 되었을때를 알려주는 finish라는 이벤트
+      this.logger.log(
+        `${req.ip}, ${req.method} ${req.statusCode}`,
+        req.originalUrl,
+      );
+      // 요청하는거에 대한 정보도 찍히고 반환을 했을때 실패했는지 성공했는지까지도 찍히게 된다.
+    });
+    next();
+  }
+}
